8e34fb903f7426a12d7941fabec550f90f0a03b0
Hamed Akbarzadeh <h.akbarzadh@gmail.com>
2025-07-26 15:31:15 +0330

    add : add refresh-token.guard.ts

--- src/@auth/controllers/auth/auth.service.ts
+++ src/@auth/controllers/auth/auth.service.ts
@@ -92,4 +92,8 @@ export class AuthService {
         }
     }
 
+    async getRefreshToken() {
+        
+    }
+
 }
---
+++ src/@auth/types/guards/refresh-token.guard.ts
@@ -0,0 +1,57 @@
+import { JwtService } from '@nestjs/jwt';
+import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from "@nestjs/common";
+import { Request } from "express";
+import { Observable } from "rxjs";
+import { ConfigService } from '@nestjs/config';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UserEntity } from 'src/@orm/models/auth/user.entity';
+import { Repository } from 'typeorm';
+import * as bcrypt from 'bcrypt'
+
+@Injectable()
+export class RefreshTokenGuard implements CanActivate {
+
+    constructor(
+        private jwtService: JwtService,
+        private configService: ConfigService,
+        @InjectRepository(UserEntity) private userRepo: Repository<UserEntity>
+    ) { }
+
+    async canActivate(context: ExecutionContext): Promise<boolean> {
+        const ctx = context.switchToHttp()
+        const req = ctx.getRequest()
+        const token = this.extractRefreshTokenFromCookie(req)
+
+        if (!token) {
+            throw new UnauthorizedException('please enter the refresh_token in cookie')
+        }
+
+        try {
+            const payload = await this.jwtService.verifyAsync(token, {
+                secret: this.configService.get('JWT_REFRESH_TOKEN_SECRET')
+            })
+
+            const user = await this.userRepo.findOneBy({ id: payload.sub });
+            if (!user || !user.currentHashedRefreshToken) {
+                throw new UnauthorizedException('Invalid user or missing refresh token');
+            }
+
+            const isMatch = await bcrypt.compare(token, user.currentHashedRefreshToken);
+            if (!isMatch) {
+                throw new UnauthorizedException('Refresh token mismatch');
+            }
+
+            req['user'] = user;
+            return true;
+
+        } catch (error) {
+            throw new UnauthorizedException('Refresh token is invalid');
+        }
+    }
+
+    private extractRefreshTokenFromCookie(req: Request) {
+        const refresh_token = req.cookies?.refresh_token
+        return refresh_token
+    }
+
+}
