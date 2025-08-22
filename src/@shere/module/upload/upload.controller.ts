c0826a3ff415509c5549948565901ed0218bbb61
Hamed Akbarzadeh <h.akbarzadh@gmail.com>
2025-08-22 21:54:32 +0330

    update: add the removeFile method

--- src/@shere/module/upload/upload.controller.ts
+++ src/@shere/module/upload/upload.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Post, Req, UploadedFile, UseInterceptors } from '@nestjs/common';
+import { Controller, Delete, NotFoundException, Param, Post, Req, UploadedFile, UseInterceptors } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { Request } from 'express';
-
+import { join } from 'path';
+import * as fs from 'fs'
 @Controller()
 export class UploadController {
 
@@ -13,5 +14,18 @@ export class UploadController {
         }
         const url = `/uploaded/${req.query.folder || 'default'}/${file.filename}`;
         return { success: true, url }
+    }
+
+    @Delete('remove/:filename')
+    removeFile(@Param('filename') filename: string, @Req() req: Request) {
+        const filePath = join(process.cwd(), `public/uploads/${req.query.folder || 'default'}`, filename);
+
+        if (!fs.existsSync(filePath)) {
+            throw new NotFoundException('File not found');
+        }
+
+        fs.unlinkSync(filePath);
+        return { message: 'File deleted successfully' };
+
     }
 }
